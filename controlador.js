2851d4ad94fb4f208d9b89bcca65d545bf1be1de
luism <luismontoya712@gmail.com>
2021-08-25 08:44:03 -0500

    Se crean referencias del dom

--- controlador.js
+++ controlador.js
@@ -1,1 +1,44 @@
-console.log("Quiubo pues");
+let boton1=document.getElementById("botonnext");
+let boton2=document.getElementById("botonprevious");
+let foto1=document.getElementById("21savage1");
+let titulo1=document.getElementById("titulo1");
+let audio1=document.getElementById("audio1");
+let foto1=document.getElementById("21savage1");
+let titulo1=document.getElementById("titulo1");
+let audio1=document.getElementById("audio1");
+
+boton1.addEventListener("click",perroGuardian);
+
+function perroGuardian() {
+    titulo1.textContent="BrandNewDraco";
+    foto1.src="img/21savage2.jpg";
+    audio1.src="audio/BrandNewDraco.mp3";
+
+    titulo2.textContent="BrandNewDraco";
+    foto1.src="img/21savage2.jpg";
+    audio1.src="audio/BrandNewDraco.mp3";
+
+    titulo1.textContent="BrandNewDraco";
+    foto1.src="img/21savage2.jpg";
+    audio1.src="audio/BrandNewDraco.mp3";
+
+    titulo1.textContent="BrandNewDraco";
+    foto1.src="img/21savage2.jpg";
+    audio1.src="audio/BrandNewDraco.mp3";
+
+    titulo1.textContent="BrandNewDraco";
+    foto1.src="img/21savage2.jpg";
+    audio1.src="audio/BrandNewDraco.mp3";
+
+    titulo1.textContent="BrandNewDraco";
+    foto1.src="img/21savage2.jpg";
+    audio1.src="audio/BrandNewDraco.mp3";
+}
+
+boton2.addEventListener("click",perroGuardian2);
+
+function perroGuardian2() {
+
+}
+
+
